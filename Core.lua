ec1769db16b2f6d179602a8641141f5283c7e7f4
Hotel2Oscar <waters.daniel.c@gmail.com>
2020-10-22 15:49:30 -0400

    cleanup and database pruning

--- Core.lua
+++ Core.lua
@@ -3,82 +3,93 @@ ReagentProfessions = LibStub("AceAddon-3.0"):NewAddon("ReagentProfessions", "Ace
 
 local debug = true
 
+local function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+
+        for k,v in pairs(o) do
+            if type(k) ~= 'number' then k = '"'..k..'"' end
+            
+            s = s .. '['..k..'] = ' .. dump(v) .. ','
+        end
+        
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
 function ReagentProfessions:Debug(message)
-    if debug then print(message) end
+    if debug then self:Print(message) end
 end
 
 function ReagentProfessions:OnInitialize()
-    -- Code that you want to run when the addon is first loaded goes here.
     self.db = LibStub("AceDB-3.0"):New("ReagentProfessionsDB")
 end
 
 function ReagentProfessions:OnEnable()
-    -- Called when the addon is enabled
-    self:Debug("ReagentProfessions enabled!")
-
-    -- self:RegisterEvent("TRADE_SKILL_SHOW")
+    -- NOTE: this fires twice when window is opened
     self:RegisterEvent("TRADE_SKILL_LIST_UPDATE")
+    self:RegisterEvent("PLAYER_LOGOUT")
 
     self:RawHookScript(GameTooltip, "OnTooltipSetItem", "AttachTooltip")
     self:RawHookScript(ItemRefTooltip, "OnTooltipSetItem", "AttachTooltip")
 end
 
-function ReagentProfessions:OnDisable()
-    -- Called when the addon is disabled
-    self:Debug("ReagentProfessions disabled!")
+function ReagentProfessions:OnDisable() end
+
+function ReagentProfessions:PLAYER_LOGOUT()
+    self:PruneDB()    
 end
 
-local function dump(o)
-    if type(o) == 'table' then
-       local s = '{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} '
-    else
-       return tostring(o)
-    end
- end
+function ReagentProfessions:PruneDB()
+    local db = self.db.global.reagents
 
-function ReagentProfessions:TRADE_SKILL_SHOW()
-    self:Debug("ReagentProfessions trade skill show fired!")
-    self:ProcessRecipes()
+    for reagent, professions in pairs(db) do
+        local stillNeeded = false
+
+        for profession, needed in pairs(professions) do
+            if needed then
+                stillNeeded = true
+            else
+                db[reagent][profession] = nil
+            end
+        end
+
+        if not stillNeeded then
+            db[reagent] = nil
+        end
+    end
 end
 
 function ReagentProfessions:TRADE_SKILL_LIST_UPDATE()
-    self:Debug("ReagentProfessions trade skill list update fired!")
     self:ProcessRecipes()
 end
 
 function ReagentProfessions:ProcessRecipes()
     local _, skillLineDisplayName, _, _, _, _, parentSkillLineDisplayName = C_TradeSkillUI.GetTradeSkillLine()
 
-    -- TODO: use timer to queue up reading recipes
-
     local profession = "Unknown"
 
     if not skillLineDisplayName then 
-        self:Debug("no skill in queue") 
-        return 
+        return
     else
         profession = skillLineDisplayName
 
-        self:Debug(skillLineDisplayName)
-
         if parentSkillLineDisplayName then
             profession = parentSkillLineDisplayName
-
-            self:Debug("parent: " .. parentSkillLineDisplayName)
         end
     end
 
     if not C_TradeSkillUI.IsTradeSkillReady() then 
-        self:Debug("trade skill not ready")
         return 
     end
 
     local db = self.db.global
+
+    if not db["reagents"] then db["reagents"] = {} end
+
+    db = db.reagents
 
     for reagent, _ in pairs(db) do
         db[reagent][profession] = false
@@ -86,12 +97,8 @@ function ReagentProfessions:ProcessRecipes()
 
     local recipeIDs = C_TradeSkillUI.GetAllRecipeIDs()
 
-    self:Debug(#recipeIDs)
-
     for key, recipeID in pairs(recipeIDs) do
         local recipeInfo = C_TradeSkillUI.GetRecipeInfo(recipeID)
-
-        -- self:Debug("processing " .. recipeID .. " " .. recipeInfo.name)
 
         for reagentIndex = 1, C_TradeSkillUI.GetRecipeNumReagents(recipeID) do
             local reagentName = C_TradeSkillUI.GetRecipeReagentInfo(recipeID, reagentIndex)
@@ -102,8 +109,6 @@ function ReagentProfessions:ProcessRecipes()
                 if not db[reagentName][profession] then
                     db[reagentName][profession] = true
                 end
-            else
-                self:Debug("no reagent name for " .. recipeInfo.name .. " " .. reagentIndex)
             end
         end
     end
@@ -114,7 +119,7 @@ function ReagentProfessions:AttachTooltip(tooltip, ...)
 
     itemName, _ = tooltip:GetItem();
 
-    local db = self.db.global
+    local db = self.db.global.reagents
 
     if not db[itemName] then return end
 
