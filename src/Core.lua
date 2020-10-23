6285931293d81eff10a1cf2620e621699fbce3c6
Hotel2Oscar <waters.daniel.c@gmail.com>
2020-10-22 22:41:27 -0400

    Added expansion category to tooltip info

--- src/Core.lua
+++ src/Core.lua
@@ -1,22 +1,6 @@
 TradeSkillReagents = LibStub("AceAddon-3.0"):NewAddon("TradeSkillReagents", "AceConsole-3.0", "AceEvent-3.0", "AceHook-3.0")
 
-local debug = true
-
-local function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-
-        for k,v in pairs(o) do
-            if type(k) ~= 'number' then k = '"'..k..'"' end
-            
-            s = s .. '['..k..'] = ' .. dump(v) .. ','
-        end
-        
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
+local debug = false
 
 function TradeSkillReagents:Debug(message)
     if debug then self:Print(message) end
@@ -29,36 +13,9 @@ end
 function TradeSkillReagents:OnEnable()
     -- NOTE: this fires twice when window is opened
     self:RegisterEvent("TRADE_SKILL_LIST_UPDATE")
-    self:RegisterEvent("PLAYER_LOGOUT")
 
     self:RawHookScript(GameTooltip, "OnTooltipSetItem", "AttachTooltip")
     self:RawHookScript(ItemRefTooltip, "OnTooltipSetItem", "AttachTooltip")
-end
-
-function TradeSkillReagents:OnDisable() end
-
-function TradeSkillReagents:PLAYER_LOGOUT()
-    self:PruneDB()    
-end
-
-function TradeSkillReagents:PruneDB()
-    local db = self.db.global.reagents
-
-    for reagent, professions in pairs(db) do
-        local stillNeeded = false
-
-        for profession, needed in pairs(professions) do
-            if needed then
-                stillNeeded = true
-            else
-                db[reagent][profession] = nil
-            end
-        end
-
-        if not stillNeeded then
-            db[reagent] = nil
-        end
-    end
 end
 
 function TradeSkillReagents:TRADE_SKILL_LIST_UPDATE()
@@ -67,7 +24,7 @@ end
 
 function TradeSkillReagents:ProcessRecipes()
     local _, skillLineDisplayName, _, _, _, _, parentSkillLineDisplayName = C_TradeSkillUI.GetTradeSkillLine()
-
+    
     local profession = "Unknown"
 
     if not skillLineDisplayName then 
@@ -79,25 +36,36 @@ function TradeSkillReagents:ProcessRecipes()
             profession = parentSkillLineDisplayName
         end
     end
-
+    
     if not C_TradeSkillUI.IsTradeSkillReady() then 
         return 
     end
 
     local db = self.db.global
-
+    
+    local categories = {}
+    
+    for _, categoryID in pairs({C_TradeSkillUI.GetCategories()}) do
+        categories[categoryID] = C_TradeSkillUI.GetCategoryInfo(categoryID).name
+    end
+    
     if not db["reagents"] then db["reagents"] = {} end
 
     db = db.reagents
-
-    for reagent, _ in pairs(db) do
-        db[reagent][profession] = false
-    end
 
     local recipeIDs = C_TradeSkillUI.GetAllRecipeIDs()
 
     for key, recipeID in pairs(recipeIDs) do
         local recipeInfo = C_TradeSkillUI.GetRecipeInfo(recipeID)
+
+        local categoryInfo = C_TradeSkillUI.GetCategoryInfo(recipeInfo.categoryID)
+        local category = categories[categoryInfo.parentCategoryID]
+
+        if not category then
+            category = categories[categoryInfo.categoryID]
+        end
+
+        category = profession .. " - " .. category
 
         for reagentIndex = 1, C_TradeSkillUI.GetRecipeNumReagents(recipeID) do
             local reagentName = C_TradeSkillUI.GetRecipeReagentInfo(recipeID, reagentIndex)
@@ -105,8 +73,8 @@ function TradeSkillReagents:ProcessRecipes()
             if reagentName then 
                 db[reagentName] = db[reagentName] or {}
                 
-                if not db[reagentName][profession] then
-                    db[reagentName][profession] = true
+                if not db[reagentName][category] then
+                    db[reagentName][category] = true
                 end
             end
         end
