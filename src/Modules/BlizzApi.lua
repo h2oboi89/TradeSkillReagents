b3a6270f0b366114d6ce945e226e8e23c199e3b6
Waters <h2oboi89@gmail.com>
2026-03-10 23:31:14 -0400

    better wrapping of blizz api

--- src/Modules/BlizzApi.lua
+++ src/Modules/BlizzApi.lua
@@ -2,6 +2,16 @@ local BlizzApi = TradeSkillReagentsModules:Create("BlizzApi");
 
 local Logger = TradeSkillReagentsModules:Import("Logger");
 
+function checkListForNil(list)
+    for _, v in ipairs(list) do
+        if v == nil then
+            return false;
+        end
+    end
+
+    return true;
+end
+
 function BlizzApi:GetTradeSkillLine()
     local retOk, r1, r2, r3 = pcall(GetTradeSkillLine);
 
@@ -15,11 +25,18 @@ function BlizzApi:GetTradeSkillLine()
     local currentLevel = r2;
     local maxLevel = r3;
 
-    return {
-        success = true, 
-        values = { tradeskillName, currentLevel, maxLevel },
+    local result = {
+        values = { },
         error = nil,
     };
+
+    table.insert(result.values, tradeskillName);
+    table.insert(result.values, currentLevel);
+    table.insert(result.values, maxLevel);
+
+    result.success = checkListForNil(result.values);
+
+    return result;
 end
 
 function BlizzApi:GetNumTradeSkills()
@@ -61,11 +78,26 @@ function BlizzApi:GetTradeSkillInfo(skillIndex)
     local maxRank = r10;
     local startingRank = r11;
 
-    return {
-        success = true,
-        values = { skillName, skillType, numAvailable, isExpanded, altVerb, numSkillUps, indentLevel, showProgressBar, currentRank, maxRank, startingRank },
+    local result = {
+        values = { },
         error = nil,
     };
+
+    table.insert(result.values, skillName);
+    table.insert(result.values, skillType);
+    table.insert(result.values, numAvailable);
+    table.insert(result.values, isExpanded);
+    table.insert(result.values, altVerb);
+    table.insert(result.values, numSkillUps);
+    table.insert(result.values, indentLevel);
+    table.insert(result.values, showProgressBar);
+    table.insert(result.values, currentRank);
+    table.insert(result.values, maxRank);
+    table.insert(result.values, startingRank);
+
+    result.success = checkListForNil(result.values);
+
+    return result;
 end
 
 function BlizzApi:GetTradeSkillNumReagents(tradeSkillRecipeId)
@@ -100,11 +132,19 @@ function BlizzApi:GetTradeSkillReagentInfo(tradeSkillRecipeId, reagentId)
     local reagentCount = r3;
     local playerReagentCount = r4;
 
-    return {
-        success = true,
-        values = { reagentName, reagentTexture, reagentCount, playerReagentCount },
+    local result = {
+        values = { },
         error = nil,
     };
+
+    table.insert(result.values, reagentName);
+    table.insert(result.values, reagentTexture);
+    table.insert(result.values, reagentCount);
+    table.insert(result.values, playerReagentCount);
+
+    result.success = checkListForNil(result.values);
+
+    return result;
 end
 
 function BlizzApi:GetCraftName()
@@ -118,11 +158,16 @@ function BlizzApi:GetCraftName()
 
     local craftName = r1;
 
-    return {
-        success = true,
-        values = { craftName },
+    local result = {
+        values = { },
         error = nil,
     };
+
+    table.insert(result.values, craftName);
+
+    result.success = checkListForNil(result.values);
+
+    return result;
 end
 
 function BlizzApi:GetNumCrafts()
@@ -160,11 +205,22 @@ function BlizzApi:GetCraftInfo(skillIndex)
     local trainingPointCost = r6;
     local requiredLevel = r7;
 
-    return {
-        success = true,
-        values = { craftName, craftSubSpellName, craftType, numAvailable, isExpanded, trainingPointCost, requiredLevel },
+    local result = {
+        values = { },
         error = nil,
     };
+
+    table.insert(result.values, craftName);
+    table.insert(result.values, craftSubSpellName);
+    table.insert(result.values, craftType);
+    table.insert(result.values, numAvailable);
+    table.insert(result.values, isExpanded);
+    table.insert(result.values, trainingPointCost);
+    table.insert(result.values, requiredLevel);
+
+    result.success = checkListForNil(result.values);
+
+    return result;
 end
 
 function BlizzApi:GetCraftNumReagents(craftRecipeId)
@@ -199,9 +255,17 @@ function BlizzApi:GetCraftReagentInfo(craftRecipeId, reagentId)
     local reagentCount = r3;
     local playerReagentCount = r4;
 
-    return {
-        success = true,
-        values = { reagentName, reagentTexture, reagentCount, playerReagentCount },
+    local result = {
+        values = { },
         error = nil,
     };
+
+    table.insert(result.values, reagentName);
+    table.insert(result.values, reagentTexture);
+    table.insert(result.values, reagentCount);
+    table.insert(result.values, playerReagentCount);
+
+    result.success = checkListForNil(result.values);
+
+    return result;
 end
